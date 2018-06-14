d9c3b7787c212df297a1ab73e7679df1ca656ab0
Li Yi <jasmin.liyi97@gmail.com>
2018-06-13 19:01:36 -0700

    Add method to count # of tickets given certain conditions

--- src/utils/OracleConnection.java
+++ src/utils/OracleConnection.java
@@ -11,22 +11,13 @@ public class OracleConnection {
     /*
      * connects to Oracle database named ug using user supplied username and password
      */
-    public Connection connect(String username, String password) throws SQLException
-    {
+    public void connect() {
         String connectURL = "jdbc:oracle:thin@dbhost.ugrad.cs";
-
-        try
-        {
-            //conn = DriverManager.getConnection(connectURL,username,password);
+        try {
             conn = DriverManager.getConnection(connectURL,"ora_p9n0b","a10804152");
-
             System.out.println("\nConnected to Oracle!");
-            return conn;
-        }
-        catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             System.out.println("Message: " + ex.getMessage());
-            throw ex;
         }
     }
 }
--- src/utils/TicketDB.java
+++ src/utils/TicketDB.java
@@ -1,4 +1,28 @@
 package utils;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class TicketDB {
+
+    private OracleConnection oracle;
+
+    // This returns the number of tickets show given the Movie Title and the start time of the Showtime
+    public int ticketSoldPerMoviePerShowtime (String movieTitlle, String showTime) throws SQLException {
+        oracle.connect();
+        try {
+            PreparedStatement ps = oracle.conn.prepareStatement(
+                    "SELECT COUNT(*) FROM TICKET " +
+                    "WHERE TITLE = ? AND START_TIME = ?");
+            ps.setString(1, movieTitlle);
+            ps.setString(2, showTime);
+            ResultSet rs = ps.executeQuery();
+
+            return rs.getFetchSize();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            throw ex;
+        }
+    }
 }
