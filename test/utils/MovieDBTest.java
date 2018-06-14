8037bc7b4eb854f62901a7f23b90e30b246d55e2
Li Yi <jasmin.liyi97@gmail.com>
2018-06-14 08:19:40 -0700

    Added JUnit test

--- test/utils/MovieDBTest.java
+++ test/utils/MovieDBTest.java
@@ -12,6 +12,6 @@ public class MovieDBTest {
         movieDB = new MovieDB();
         MovieInfo actual = movieDB.getMovieInfo("Tag");
         MovieInfo expected = new MovieInfo(100, "Comedy", "14A");
-        Assert.assertEquals(expected, actual);
+        Assert.assertTrue(expected.equals(actual));
     }
 }
