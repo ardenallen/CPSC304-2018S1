3e6c9ed71f29387411b3231437f28b09d55198f2
Li Yi <jasmin.liyi97@gmail.com>
2018-06-14 19:29:32 -0700

    Moved getTicketSold method to model pkg

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,10 +1,24 @@
 package model;
 
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Employee extends User {
     public Employee(int userId) {
         super("employee", userId);
     }
+
+    public int ticketSoldPerMoviePerShowtime (String movieTitle, String showTime) {
+        int ticketSold = -1;
+        try {
+            PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) FROM Ticket " +
+                    "WHERE TITLE = ? AND START_TIME = ?");
+        } catch (SQLException ex) {
+            System.out.println("Message: " + ex.getMessage());
+        }
+        return ticketSold;
+    }
+
+
+
 }
