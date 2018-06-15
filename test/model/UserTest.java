bf60aac190c156f816c2c19ab4418a9e6da7e4be
Li Yi <jasmin.liyi97@gmail.com>
2018-06-14 19:10:37 -0700

    Added JUnist test for User

---
+++ test/model/UserTest.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTest {
+    private User user = new User("TestUser", 1234);
+
+    @Test
+    public void testUser() {
+        Assert.assertTrue(1 == 1);
+    }
+}
