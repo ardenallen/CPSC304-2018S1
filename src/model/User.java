3b375d967f6d58a2ce3fae115df53140a1669b57
Li Yi <jasmin.liyi97@gmail.com>
2018-06-14 19:22:33 -0700

    Connect method in user (super) class

--- src/model/User.java
+++ src/model/User.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class User {
+    public Connection conn;
     private String userClass;
     private int userId;
 
@@ -18,6 +19,20 @@ public class User {
         return userClass;
     }
 
+    // Connects to Oracle database with hardcoded username and password
+    public void connect() {
+        String connectURL = "jdbc:oracle:thin:@localhost:1522:ug";
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            conn = DriverManager.getConnection(connectURL,"ora_p9n0b","a10804152");
+            System.out.println("\nConnected to Oracle!");
+        } catch (SQLException ex) {
+            System.out.println("Message: " + ex.getMessage());
+            ex.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
     public void setUserClass(String newUserClass) { this.userClass = newUserClass; }
 
     public Integer getUserId() {
@@ -25,4 +40,8 @@ public class User {
     }
 
     public void setUserId(int newUserId) { this.userId = newUserId; }
+
+    //public MovieInfo getMovieInfo(String ) {
+
+    //}
 }
